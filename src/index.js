8dabd8be9f584d1c5d9a2beb733d3b59d78111c9
Lucas Favaro <lucasf4varo@gmail.com>
2021-11-23 13:41:28 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,25 +3,7 @@ const express = require('express');
 
 const app = express();
 
-/**
- * Get- Buscar uma informação dentro do servidor
-Post - Inserir uma informação no servidor
-Put - Alterar uma informação no servidor
-Patch - Alterar uma informação especifica
-Delete - Deletar uma informação no servidor
 
-
-
-Tipos de Parâmetros
-
-Route Params => Identificar um recurso para editar/deletar/buscar
-
-Query Params => Paginação / Filtro 
-
-Body Params => Objetos para inserção ou alteração de algum recurso. (JSON)
-
-
- */
 app.use(express.json());
 
 app.get("/courses", (request, response) => {
